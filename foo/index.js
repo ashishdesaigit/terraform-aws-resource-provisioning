564756add720a7ea6639962b7916260035c36d57
ashishdesai <ashishdesai21@gmail.com>
2020-06-15 23:36:56 +0530

    tf source file hash added

--- foo/index.js
+++ foo/index.js
@@ -2,6 +2,6 @@ const {uuid} = require ('uuid');
 
 exports.handler = async (event) => {
 
-    return "This is simplest lambda for FOO :-" + uuid.v4();
+    return "This is dumbest lambda for FOO :-" + uuid.v4();
 
 }
--- infra/main.tf
+++ infra/main.tf
@@ -40,4 +40,6 @@ resource "aws_lambda_function" "foo_function" {
     runtime         = "nodejs12.x"
     role            = aws_iam_role.iam_for_lambda.arn
 
+    source_code_hash = "${filebase64sha256("foo.zip")}"
+
 }
