ca7c16868354cbd9346daed541820c02871bd12c
ashishdesai <ashishdesai21@gmail.com>
2020-06-16 00:11:52 +0530

    workflow file changed

--- infra/main.tf
+++ infra/main.tf
@@ -40,6 +40,4 @@ resource "aws_lambda_function" "foo_function" {
     runtime         = "nodejs12.x"
     role            = aws_iam_role.iam_for_lambda.arn
 
-    source_code_hash = filebase64sha256("foo.zip")
-
 }
